62bcd7ee11d18a1ad3b9de75d0a42f25b53592db
Reyhan Vivaldi Adrian <reyhan.vivaldi@ui.ac.id>
2022-09-15 16:02:30 +0700

    tutorial 2 done

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
-
+# Tutorial 1
 data_barang_wishlist = BarangWishlist.objects.all()
 context = {
     'list_barang': data_barang_wishlist,
@@ -12,3 +13,21 @@ context = {
 
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+
+
+# Tutorial 2
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data_barang_wishlist), content_type="application/xml")
+def show_json(request):
+    return HttpResponse(serializers.serialize("json", data_barang_wishlist), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
+
